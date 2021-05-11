50e18d2def784663d3110e6f85b3273f5498c4ed
Sebastian Gracia <sebasgraciavalderrama@users.noreply.github.com>
2021-05-11 18:03:55 -0500

    Reducer to get min value of an array

--- reducer/reducer.js
+++ reducer/reducer.js
@@ -4,3 +4,14 @@ const prices = [1,2,3,4,5,6,7,8,9]
 const reducer = (accumulator, currentValue) => accumulator + currentValue
 
 console.log(prices.reduce(reducer))
+
+//To get the min value...
+
+const reducerMin = (min, currentPrice) => {
+  if (prince > min) {
+    return price
+  }
+  return min;
+}
+
+prices.reduceMin(reducerMin);
